aaceae8cc4f0190f66652d74c2fae31dcbef0767
diegocairone <diegocairone@gmail.com>
2017-01-03 18:44:16 -0300

    Servicio de PAISES - Buscar todos

--- appexample/src/main/java/com/cairone/appexample/services/PaisService.java
+++ appexample/src/main/java/com/cairone/appexample/services/PaisService.java
@@ -1,7 +1,10 @@
 package com.cairone.appexample.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cairone.appexample.entities.PaisEntity;
 import com.cairone.appexample.repositories.PaisRepository;
@@ -11,8 +14,13 @@ public class PaisService {
 
 	@Autowired private PaisRepository paisRepository = null;
 	
-	public PaisEntity buscarPorID(Integer id) {
-		
+	@Transactional(readOnly = true)
+	public PaisEntity buscarPorID(Integer id) {		
 		return paisRepository.findOne(id);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<PaisEntity> buscarPaises() {
+		return paisRepository.findAll();
 	}
 }
